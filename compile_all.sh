1413cf29c0bf62682b6474c001cfd3e4e9e7dcae
Joshua Chu <jchutrue@gmail.com>
2017-06-30 10:48:19 -0500

    Fixed error in calculation when determining how frequently to log % extraction completed

--- compile_all.sh
+++ compile_all.sh
@@ -8,4 +8,4 @@ gcc src/read_config.c src/diskio_linux.c -o bin/read_config
 gcc src/write_config.c src/diskio_linux.c -o bin/write_config
 gcc src/card_enable.c src/diskio_linux.c -o bin/card_enable
 gcc src/pcheck.c src/diskio_linux.c -o bin/pcheck
-gcc src/sd_card_extract.c src/diskio_linux.c -o bin/sd_card_extract
+gcc src/sd_card_extract.c src/diskio_linux.c -o bin/sd_card_extract -lm
